4747f0da272c0aa010214a21da9f1bcf433a5424
rohitreddy12 <166005144+rohitreddy12@users.noreply.github.com>
2024-04-04 10:57:00 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <h1>WELCOME TO MOVIE SEARCH ENGINE</h1>
+      </>
+      <input type="text" placeholder="Search..."></input> <br></br>
+      <button type="button" style={{backgroundColor:"lavender "}}>Click here</button>
+    </div>
+    
+  );
+}
+
+
+
+
+
+export default App;
