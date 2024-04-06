56c27fa11042202c1aed00698cd744a2e58e0524
rohit reddy <rohit.reddy@zapcg.com>
2024-04-06 02:00:55 -0700

    Implemented a search bar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
+import { useEffect, useState } from "react";
 
-import { useEffect,useState} from "react";
+function App(){
+
+  const [search,setSearch]=useState("")
+
+  
+  // useEffect(() => {() => {}},[search])
+
+  useEffect(() => {
+    if(search.length>=3){
+      fetch(``)
+      .then(response => response.json())
+      .then(data => console.log(data))
+    } 
+  },[])
+
+  
 
 
-function App() {
-
-  const movie_details= (xy) =>{
-    fetch("https://api.themoviedb.org/3/search/movie?query=${xy}&api_key=9e03485c60de4a3d6d15eace88f6e026")
-    .then(Response => Response.json())
-    .then(json => console.log(json))
-
-  }
-
-  const [inputText,setInputText]=useState("")
-
-  useEffect( ()=>{
-    movie_details(setInputText)
-  },[]
- )
-
-  return (
-    <div className="App">
-      <h1>WELCOME TO MOVIE SEARCH ENGINE</h1>
-      <form>
-        <label for="search">Enter the name of the movie</label>
-        <input type="text" id="search" onChange={e => setInputText(e.target.value)} value={inputText} placeholder="Search..."></input>
-      </form>
+  return(
+    <div>
+      <h1>Welcome to Movie Search</h1>
+      <input type="text" placeholder="Search..." onChange={e=>{setSearch(e.target.value)}} value={search}></input>
     </div>
     
-    
-  );
+  )
 
 }
+
 
 export default App;
 
 
+/*onChange is an event that is used to recognise and identify the change.when used in inout it identifies 
+whenever a user enters a char*/
+// .target is used to identify the change and .value is used to get the changed value
 
+// useState and useEffect are ReactHooks
