ba8675b57e8b220655ff1760f8bc2f0a58014779
rohitreddy12 <rohit.reddy@zapcg.com>
2024-04-06 10:33:02 -0700

    added the url

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ function App(){
 
   const [search,setSearch]=useState("")
 
-
-  useEffect(() => {
+  function apiCall(movie){
     if(search.length>=3){
-      fetch(``)
+      fetch(`https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=9e03485c60de4a3d6d15eace88f6e026`)
       .then(response => response.json())
       .then(data => console.log(data))
     } 
+  }
+
+  useEffect(() => {
+    apiCall(search) 
   },[search])
 
 
   return(
     <div>
       <h1>Welcome to Movie Search</h1>
-      <input type="text" placeholder="Search..." onChange={e=>{setSearch(e.target.value)}} value={search}></input>
-    </div>
-    
+      <input type="text" placeholder="Search..." onChange={e=>{setSearch(e.target.value)}} value={search}></input> 
+    </div>   
   )
 
 }
