692ff91bcba451612236fe535e64206f67b3e234
rohitreddy12 <rohit.reddy@zapcg.com>
2024-05-09 08:09:19 -0700

    removing from favourites

---
+++ src/Components/removeFavourite.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export function removeFavourite(props:number) {
+    const options = {
+        method: 'POST',
+        headers: {
+          accept: 'application/json',
+          'content-type': 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwN2M3Yjc2MzQ3MTRiZDExMzU4ZjhlYjMwZmZmNzEwMiIsInN1YiI6IjY2MTAyNzcxZDg2MWFmMDE2NGYzYTZiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6v1iORQR-M6zqXrZfqaUdBEjJrLT2l5c6X6j6en5HsM'
+        },
+        body: JSON.stringify({media_type: 'movie', media_id: props, favorite: false})
+      };
+      
+    const removeFav = () => {
+        fetch('https://api.themoviedb.org/3/account/21190820/favorite', options)
+    }
+
+    removeFav();
+}
+
