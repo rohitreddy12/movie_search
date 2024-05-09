797326d57fe4a818e0399cc677da0a912b2f4981
rohitreddy12 <rohit.reddy@zapcg.com>
2024-05-09 08:12:23 -0700

    Implemented favourites page

--- src/Pages/Favourites.tsx
+++ src/Pages/Favourites.tsx
@@ -1,46 +1,63 @@
-import React, { useEffect, useState } from 'react'
-import NavScrollExample from '../Components/navbar'
-import './Favourites.css'
+
+import { useEffect, useState } from 'react';
+import NavBar from '../Components/navbar';
+import './Favourites.css';
+import { Card, Col, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
 
 
+interface Details {
+    poster_path: string,
+    title: string,
+    id: string
+}
 
 function Favourites() {
 
-    // const [favPaths,setFavPaths]=useState([])
+    const [favItems, setFavItems] = useState<Details[]>([])
 
-    let favResults:string[]=[]
-    
-    const storedFav = localStorage.getItem('fav')
-    
-    const storedResults=localStorage.getItem('results')
 
-    if(storedFav && storedResults){
-        
-        let fav=JSON.parse(storedFav)
-        let results=JSON.parse(storedResults)
-        
-        for(let i = 0;i<fav.length ; i++){
-            results.map((det:any) => {
-                if(det.id==fav[i]){
-                    favResults.push(det.poster_path)
-                }
-            })
-            
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwN2M3Yjc2MzQ3MTRiZDExMzU4ZjhlYjMwZmZmNzEwMiIsInN1YiI6IjY2MTAyNzcxZDg2MWFmMDE2NGYzYTZiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6v1iORQR-M6zqXrZfqaUdBEjJrLT2l5c6X6j6en5HsM'
         }
+    };
 
+    const favourtiesList = () => {
+        fetch('https://api.themoviedb.org/3/account/21190820/favorite/movies?language=en-US&page=1&sort_by=created_at.asc', options)
+            .then((response) => response.json())
+            .then((data): void => {
+                const imgParse: Details[] = data.results.map((item: Details) => ({
+                    poster_path: item.poster_path,
+                    title: item.title,
+                    id: item.id
+                }))
+                setFavItems(imgParse)
+            })
     }
 
-    // console.log(favPaths)    
 
-    
+    useEffect(() => {
+        favourtiesList();
+    }, [])
+
+
     return (
         <div>
-            <div><NavScrollExample /></div>
-            <div id='favImg'>
-                {favResults.map((path) =>
-                    <div><img alt='Img is Loading' src={`https://image.tmdb.org/t/p/w500${path}`}></img></div>
-                )} 
-            </div> 
+            <NavBar />
+            <div id='heading'>Favourited Movies</div>
+            <div className="favImgs">
+                {favItems.map((item) =>
+                    <>
+                        <Link to={`/movie_details/${item.id}`}>
+                            <img id={item.id} src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} alt='Image' />
+                        </Link>
+                    </>
+                )}
+            </div>
         </div>
 
 
@@ -51,20 +68,3 @@ export default Favourites
 
 
 
-// const favPaths:string[]=[]
-
-    // if (storedFav && storedImgDet) {
-    //     const imgDet = JSON.parse(storedImgDet)
-    //     const fav = JSON.parse(storedFav)
-    //     fav.map(
-    //         (id:string) =>{
-    //             imgDet.map(
-    //                 (element:ImgDet) => {
-    //                     if(id===element.id){
-    //                         favPaths.push(element.poster_path)
-    //                     }
-    //                 }
-    //             )
-    //         }
-    //     )
-    // }
