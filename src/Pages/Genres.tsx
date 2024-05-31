f0235ef087dfbbf20688b5ef9943ae2df7155f84
rohitreddy12 <rohit.reddy@zapcg.com>
2024-05-31 15:27:41 +0530

    Improved CSS

--- src/Pages/Genres.tsx
+++ src/Pages/Genres.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import NavBar from '../Components/navbar'
 import Button from 'react-bootstrap/Button';
 import './genre.css'
 import { Link } from 'react-router-dom';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import MovieCard from '../Components/MovieCard';
 
 interface Genre {
     id: number,
@@ -12,13 +13,22 @@ interface Genre {
 interface GenreMovies {
     id: string,
     poster_path: string
+    title:string
+}
+
+interface MovieFilter{
+    genre:number,
+    filter:string
 }
 
 function Genres() {
 
-    const [active,setActive] =useState<number>()
+    const [active, setActive] = useState<number>(28)
     const [genreMoviesList, setGenreMoviesList] = useState<GenreMovies[]>([])
     const [genresList, setGenresList] = useState<Genre[]>([])
+
+    const [movieFilter,setMovieFilter] = useState<MovieFilter>({genre:28,filter:'vote_average.desc'})
+
     const allGenres = () => {
         const options = {
             method: 'GET',
@@ -41,21 +51,15 @@ function Genres() {
         allGenres()
     }, [])
 
-    const genreMovies = (genreId: number) => {
-        const options = {
-            method: 'GET',
-            headers: {
-                accept: 'application/json',
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwN2M3Yjc2MzQ3MTRiZDExMzU4ZjhlYjMwZmZmNzEwMiIsInN1YiI6IjY2MTAyNzcxZDg2MWFmMDE2NGYzYTZiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6v1iORQR-M6zqXrZfqaUdBEjJrLT2l5c6X6j6en5HsM'
-            }
-
-        }
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=07c7b7634714bd11358f8eb30fff7102&with_genres=${genreId}`, options)
+    const genreMovies = (movieFilter:MovieFilter) => {
+               
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=07c7b7634714bd11358f8eb30fff7102&with_genres=${movieFilter.genre}&sort_by=${movieFilter.filter}`)
             .then(response => response.json())
             .then((data) => {
                 const genreArr: GenreMovies[] = data.results.map((item: GenreMovies) => ({
                     id: item.id,
-                    poster_path: item.poster_path
+                    poster_path: item.poster_path,
+                    title:item.title
                 })
                 )
                 setGenreMoviesList(genreArr)
@@ -65,35 +69,81 @@ function Genres() {
     }
 
     const handleClick = (id: number) => {
-        genreMovies(id)
+        setMovieFilter({
+            ...movieFilter,
+            genre:id
+        })
         setActive(id)
     }
-
     
+    const handleChange:React.ChangeEventHandler<HTMLSelectElement>  = (event) => {
+        setMovieFilter({
+            ...movieFilter,
+            filter:event.target.value
+        })
+    }
 
-    
+    useEffect(
+        () => {genreMovies(movieFilter)},[movieFilter]
+    )
+
+
+    const btnCustom = {
+        borderRadius: '20px'
+    }
 
     return (
-        <div className="mainPage">
-            <NavBar />
-            <div className='page'>
+        <div className="genrePage">
+            <div className='genreSideBar'>
 
-                <div className='genreButtons'>
+                <div className="genreBox">
+                    {/* <Dropdown.Toggle variant='outline-dark' id='sortbyButton'>Genres</Dropdown.Toggle>
+                    <Dropdown.Menu id='dropdownFilters'>
+                        <div className='genreButtons'>
+                        {
+                            genresList.map((elem) =>                           
+                                <Button style={btnCustom} className='genrePill' size='sm' variant='outline-secondary' id={elem.name} onClick={() => handleClick(elem.id)} active={active == elem.id}>{elem.name}</Button>              
+                            )
+                        }   
+                        </div>
+                    </Dropdown.Menu> */}
+                    <div className='genreTitle'>Genres</div>
+                    <div className='genreButtons'>
                     {
-                        genresList.map((elem) =>
-                            <Button variant='outline-secondary' id={elem.name} onClick={() => handleClick(elem.id)}  active={active==elem.id}    >{elem.name}</Button>
+                        genresList.map((elem) =>                           
+                            <Button style={btnCustom} className='genrePill' size='sm' variant='outline-secondary' id={elem.name} onClick={() => handleClick(elem.id)} active={active == elem.id}>{elem.name}</Button>              
                         )
-                    }
+                    }   
+                    </div>
                 </div>
-                <div className="genreResults">
-                    {
-                        genreMoviesList.map((elem) =>
-                            <Link to={`/movie_details/${elem.id}`}>
-                                <img src={`https://image.tmdb.org/t/p/w500${elem.poster_path}`} id={elem.id} key={elem.id} alt="Img is loading" />
-                            </Link>
-                        )
-                    }
+
+                <div className="sortFilter">
+                    <Dropdown>
+                        <Dropdown.Toggle variant='outline-dark' id='sortbyButton'>Sort</Dropdown.Toggle>
+                        <Dropdown.Menu id='dropdownFilters'>
+                            <Dropdown.Header id='dropdownHeaderTitle'>Sort Results By</Dropdown.Header>
+                            <div className='selectBox'>
+                                <select name='filters' id='filters' onChange={handleChange}>
+                                    <option value='vote_average.desc'>Rating</option>
+                                    <option value='popularity.desc'>Popularity</option>
+                                    <option value='original_title.asc'>Title(A-Z)</option>
+                                    <option value='primary_release_date.desc'>Release Date</option>
+                                </select>
+                            </div>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
+
+            </div>
+            <div className="genreResults">
+                {
+                    genreMoviesList.map((elem) =>
+                        <Link to={`/movie_details/${elem.id}`}>
+                            <MovieCard title={elem.title} poster_path={elem.poster_path} key={elem.title}/>
+                           
+                        </Link>
+                    )
+                }
             </div>
         </div>
 
