4a0f9ae219a7c0e29f1242eb33937ca60219b9bd
rohitreddy12 <rohit.reddy@zapcg.com>
2024-05-31 15:21:25 +0530

    removed favContext(not required)

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
 
-import { Route, Router, Routes } from 'react-router-dom'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Favourites from './Pages/Favourites'
 import Genres from './Pages/Genres'
 import MovieDetails from './Pages/MovieDetails'
 import NavBar from './Components/navbar'
-import MainPage from './MainPage'
-import { useState, createContext } from 'react'
-import {MyContext,FavContext} from './MyContext'
+import MainPage from './Pages/MainPage'
+import { useEffect, useState} from 'react'
+import {MyContext} from './MyContext'
+import NotFoundPage from './Pages/NotFoundPage'
 
 function App() {
-    const [isLoggedIn,setIsLoggedIn] = useState<boolean>(false)
-    const [favIds,setFavIds] = useState<string[]>([])
+      
+    const isSessionCreated = () => {
+        let sessionId = localStorage.getItem('sessionId')
+        return sessionId !== null ? true : false
+    }
+
+    const [isLoggedIn,setIsLoggedIn] = useState(isSessionCreated())
     
 
     return (
         
         
         <div>
-            <FavContext.Provider value={{favIds,setFavIds}}>
+            
             <MyContext.Provider value={{isLoggedIn ,setIsLoggedIn}}>
                 <NavBar />
             <Routes>
+                <Route path='*' element={<NotFoundPage />}></Route>
                 <Route path='/' element={<MainPage />} ></Route>
                 <Route path='/favourites' element={<Favourites />}></Route>                
                 <Route path='/genres' element={<Genres />}></Route>
                 <Route path='/movie_details/:movieId' element={<MovieDetails />}></Route>
             </Routes>
             </MyContext.Provider>
-            </FavContext.Provider>
+            
         </div>
     )
 }
