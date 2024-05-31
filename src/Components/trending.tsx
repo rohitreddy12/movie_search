713a7c1dffc1cdbf4b514d71e7531126a06237d6
rohitreddy12 <rohit.reddy@zapcg.com>
2024-05-31 15:24:41 +0530

    Minor changes

--- src/Components/trending.tsx
+++ src/Components/trending.tsx
@@ -2,57 +2,56 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import './trending.css';
 
-interface Trending{
-    id:number,
-    poster_path:string
+interface Trending {
+    id: number,
+    poster_path: string,
+    title: string
 }
 function Trending() {
 
-    const[trendingMovies,setTrendingMovies] = useState<Trending[]>([])
+    const [trendingMovies, setTrendingMovies] = useState<Trending[]>([])
 
-    const options = {
-        method: 'GET',
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwN2M3Yjc2MzQ3MTRiZDExMzU4ZjhlYjMwZmZmNzEwMiIsInN1YiI6IjY2MTAyNzcxZDg2MWFmMDE2NGYzYTZiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6v1iORQR-M6zqXrZfqaUdBEjJrLT2l5c6X6j6en5HsM'
-        }
-      };
-      
-      const getTrending = () => {
-        fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US', options)
-        .then(response => response.json())
-        .then(data => {
-            const moviesArr = data.results.map((item:Trending) => ({
-                id:item.id,
-                poster_path:item.poster_path
-            }) )
-            setTrendingMovies(moviesArr)
-        })
-        .catch(err => console.error(err));
-      } 
-    
+
+    const getTrending = () => {
+        fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=07c7b7634714bd11358f8eb30fff7102')
+            .then(response => response.json())
+            .then(data => {
+                const moviesArr = data.results.map((item: Trending) => ({
+                    id: item.id,
+                    poster_path: item.poster_path,
+                    title: item.title
+                }))
+                setTrendingMovies(moviesArr)
+            })
+            .catch(err => console.error(err));
+    }
+
     useEffect(
         () => {
             getTrending()
-        },[]
+        }, []
     )
 
-    console.log(trendingMovies)
+
     return (
-    <div className='trendingMain'>
-        <p id='trendingTag'>Trending</p>
-        <div className="trendingMovies">
-            {
-                trendingMovies.map((item:Trending) => 
-                    <Link to={`/movie_details/${item.id}`}>
-                        <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} id={String(item.id)} key={String(item.id)}  alt="Img is loading" />
-                    </Link>
-                )
-            }
+        <div className='trendingMain'>
+            <p id='trendingTag'>Trending</p>
+            <div className="trendingMovies">
+                {
+                    trendingMovies.map((item: Trending) =>
+                        <div className='items'>
+                            <Link to={`/movie_details/${item.id}`}>
+                                <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} id={String(item.id)} key={String(item.id)} alt="Img Unavailable" />
+                            </Link>
+                            <div id='movieTitle'>{item.title}</div>
+                        </div>
+
+                    )
+                }
+            </div>
+
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default Trending
